e119696f8661da86bb170937242d1551904a1c10
matsuhisa <matsuhisa+github@gmail.com>
2012-03-07 23:06:59 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+ 'Hello, Sinatra'
+end
+
+get '/page1/' do
+ 'Hello, Page1'
+end
+
+get '/page2/' do
+ 'Hello, Page2'
+end
+
+get '/page3/' do
+ 'Hello, Page3'
+end
